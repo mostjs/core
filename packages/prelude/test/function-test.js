558382b760703d752e26aea9da89a941c754d9fe
Brian Cavalier <brian@hovercraftstudios.com>
2016-02-18 21:21:20 -0500

    Add unit tests for compose and apply

--- packages/prelude/test/function-test.js
+++ packages/prelude/test/function-test.js
@@ -3,11 +3,33 @@
 import {describe, it} from 'mocha';
 import assert from 'assert';
 
-import { id } from '../src/function';
+import { id, compose, apply } from '../src/function';
 
 describe('id', () => {
-  it('should return its input', () => {
+  it('id(x) === x', () => {
     const x = {};
-    assert(id(x) === x);
+    assert.strictEqual(id(x), x);
+  });
+});
+
+describe('compose', () => {
+  it('compose(f, g)(x) === f(g(x))', () => {
+    const fx = '' + Math.random();
+    const gx = '' + Math.random();
+    const x = '' + Math.random();
+
+    const f = x => x + fx;
+    const g = x => x + gx;
+    const h = compose(f, g);
+
+    assert.strictEqual(f(g(x)), h(x));
+  });
+});
+
+describe('apply', () => {
+  it('apply(f, x) === f(x)', () => {
+    const x = Math.random();
+    const f = x => x+1;
+    assert.strictEqual(apply(f, x), f(x));
   });
 });
