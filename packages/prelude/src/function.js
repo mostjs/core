88599a0bd4ada01e14f274376c76265739819e8f
Tylor Steinberger <tlsteinberger167@gmail.com>
2016-03-11 09:35:33 -0500

    feat(function): add curry2 and curry3 implementations

--- packages/prelude/src/function.js
+++ packages/prelude/src/function.js
@@ -8,3 +8,28 @@ export const compose = (f, g) => x => f(g(x));
 
 // apply :: (a -> b) -> a -> b
 export const apply = (f, x) => f(x);
+
+// curry2 :: (a -> b -> c) -> a -> b -> c
+export function curry2(f) {
+    function curried(a, b) {
+        switch (arguments.length) {
+            case 0: return curried;
+            case 1: return b => f(a, b);
+            default:return f(a, b);
+        }
+    }
+    return curried;
+}
+
+// curry3 :: (a -> b -> c -> d) -> a -> b -> c -> d
+export function curry3(f) {
+    function curried(a, b, c) {
+        switch (arguments.length) {
+            case 0: return curried;
+            case 1: return curry2((b, c) => f(a, b, c));
+            case 2: return c => f(a, b, c);
+            default:return f(a, b, c);
+        }
+    }
+    return curried;
+}
