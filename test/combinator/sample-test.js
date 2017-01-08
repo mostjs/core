56a2f8fb5422c66747e02b1e9948c9f4f02a2f36
Brian Cavalier <brian@hovercraftstudios.com>
2017-01-07 20:18:31 -0500

    Reduce nesting, remove commented code, adjust whitespace

--- test/combinator/sample-test.js
+++ test/combinator/sample-test.js
@@ -4,7 +4,6 @@ import { spec, referee } from 'buster'
 const { describe, it } = spec
 const { assert } = referee
 
-// import {observe, empty, never, take, skip, periodic, throwError} from '../..'
 import { throwError } from '../../src/combinator/errors'
 import { just, never } from '../../src/source/core'
 import { sample } from '../../src/combinator/sample'
@@ -14,48 +13,52 @@ import { makeEvents, ticks, collectEvents } from '../helper/testEnv'
 const rint = n => Math.ceil(n * Math.random())
 
 describe('sample', () => {
-  describe('sample', () => {
-    it('should pass in the sampler value', () => {
-      const samplerValue = Math.random()
-      const justValue = Math.random()
-      const f = (a, b) => [a, b]
-      const s = sample(f, just(samplerValue), just(justValue))
-      return collectEvents(s, ticks(1)).then(events => {
-        assert.same(1, events.length)
-        assert.equals({ time: 0, value: [samplerValue, justValue] }, events[0])
-      })
-    })
+  it('should pass in the sampler value', () => {
+    const samplerValue = Math.random()
+    const justValue = Math.random()
 
-    it('should produce no events before first stream event', () => {
-      const n = rint(10)
-      const s = sample(Array, makeEvents(1, n), never())
-      return collectEvents(s, ticks(n)).then(events =>
-        assert.same(0, events.length))
-    })
+    const f = (a, b) => [a, b]
+    const s = sample(f, just(samplerValue), just(justValue))
 
-    it('should end when sampler ends', () => {
-      const n = rint(10)
-      const s = sample(Array, makeEvents(1, n), makeEvents(5, n * 2))
-      return collectEvents(s, ticks(n)).then(events =>
-        assert.same(n, events.length))
+    return collectEvents(s, ticks(1)).then(events => {
+      assert.same(1, events.length)
+      assert.equals({ time: 0, value: [samplerValue, justValue] }, events[0])
     })
+  })
 
-    it('should repeat last value after source ends', () => {
-      const n = 1 + rint(10)
-      const x = Math.random()
-      const s = sample(Array, makeEvents(1, n), just(x))
-      return collectEvents(s, ticks(n)).then(events => {
-        assert.same(n, events.length)
-        events.forEach((event, i) =>
-          assert.equals({ time: i, value: [i, x] }, event))
-      })
-    })
+  it('should produce no events before first stream event', () => {
+    const n = rint(10)
+    const s = sample(Array, makeEvents(1, n), never())
 
-    it('should error if stream errors', () => {
-      const error = new Error('fail')
-      const s = sample(Array, makeEvents(1, 1), throwError(error))
-      return collectEvents(s, ticks(1)).catch(e =>
-        assert.same(error, e))
+    return collectEvents(s, ticks(n))
+      .then(events => assert.same(0, events.length))
+  })
+
+  it('should end when sampler ends', () => {
+    const n = rint(10)
+    const s = sample(Array, makeEvents(1, n), makeEvents(5, n * 2))
+
+    return collectEvents(s, ticks(n))
+      .then(events => assert.same(n, events.length))
+  })
+
+  it('should repeat last value after source ends', () => {
+    const n = 1 + rint(10)
+    const x = Math.random()
+    const s = sample(Array, makeEvents(1, n), just(x))
+
+    return collectEvents(s, ticks(n)).then(events => {
+      assert.same(n, events.length)
+      events.forEach((event, i) =>
+        assert.equals({ time: i, value: [i, x] }, event))
     })
+  })
+
+  it('should error if stream errors', () => {
+    const error = new Error('fail')
+    const s = sample(Array, makeEvents(1, 1), throwError(error))
+
+    return collectEvents(s, ticks(1))
+      .catch(e => assert.same(error, e))
   })
 })
