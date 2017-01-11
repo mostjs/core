4759db59fdb73da714a1503a47f7a025d3ea976f
Brian Cavalier <brian@briancavalier.com>
2017-01-11 13:41:32 -0500

    feat(core): Simplify fromIterable, remove tick insertion (#17)

--- src/source/fromIterable.js
+++ src/source/fromIterable.js
@@ -1,41 +1,29 @@
 /** @license MIT License (c) copyright 2010-2016 original author or authors */
-/** @author Brian Cavalier */
-/** @author John Hann */
 
 import Stream from '../Stream'
 import { getIterator } from '../iterable'
 import PropagateTask from '../scheduler/PropagateTask'
 
-export function fromIterable (iterable) {
-  return new Stream(new IterableSource(iterable))
-}
+export const fromIterable = iterable =>
+  new Stream(new IterableSource(iterable))
 
-function IterableSource (iterable) {
-  this.iterable = iterable
-}
-
-IterableSource.prototype.run = function (sink, scheduler) {
-  return new IteratorProducer(getIterator(this.iterable), sink, scheduler)
-}
-
-function IteratorProducer (iterator, sink, scheduler) {
-  this.scheduler = scheduler
-  this.iterator = iterator
-  this.task = new PropagateTask(runProducer, this, sink)
-  scheduler.asap(this.task)
-}
-
-IteratorProducer.prototype.dispose = function () {
-  return this.task.dispose()
-}
-
-function runProducer (t, producer, sink) {
-  var x = producer.iterator.next()
-  if (x.done) {
-    sink.end(t, x.value)
-  } else {
-    sink.event(t, x.value)
+class IterableSource {
+  constructor (iterable) {
+    this.iterable = iterable
   }
 
-  producer.scheduler.asap(producer.task)
+  run (sink, scheduler) {
+    return scheduler.asap(new PropagateTask(runProducer, getIterator(this.iterable), sink))
+  }
+}
+
+function runProducer (t, iterator, sink) {
+  let r = iterator.next()
+
+  while (!r.done && this.active) {
+    sink.event(t, r.value)
+    r = iterator.next()
+  }
+
+  sink.end(t, r.value)
 }
