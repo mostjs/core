20692111461ebdacf73635e39fd4600ba84380d0
Tylor Steinberger <tlsteinberger167@gmail.com>
2017-01-15 20:27:34 -0500

    chore(type-definitions): remove deprecated reduce/observe/drain

--- type-definitions/combinator/observe.d.ts
+++
@@ -1,6 +0,0 @@
-import { Stream } from '../types';
-
-export function observe<A>(f: (a: A) => any, s: Stream<A>): Promise<any>;
-export function observe<A>(f: (a: A) => any): (s: Stream<A>) => Promise<any>;
-
-export function drain<A>(s: Stream<A>): Promise<any>;
--- type-definitions/combinator/reduce.d.ts
+++
@@ -1,6 +0,0 @@
-import { Stream } from '../types';
-
-export function reduce<A, B>(f: (b: B, a: A) => B, b: B, s: Stream<A>): Promise<B>;
-export function reduce<A, B>(f: (b: B, a: A) => B): (b: B, s: Stream<A>) => Promise<B>;
-export function reduce<A, B>(f: (b: B, a: A) => B, b: B): (s: Stream<A>) => Promise<B>;
-export function reduce<A, B>(f: (b: B, a: A) => B): (b: B) => (s: Stream<A>) => Promise<B>;
