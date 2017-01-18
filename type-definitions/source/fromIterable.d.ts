38c11ead68b9807be923efa00177ecd43ceaaebb
Tylor Steinberger <tlsteinberger167@gmail.com>
2017-01-18 18:21:56 -0500

    chore(type-definitions): fix remainder of typings

--- type-definitions/source/fromIterable.d.ts
+++ type-definitions/source/fromIterable.d.ts
@@ -1,4 +1,3 @@
 import { Stream } from '../types';
 
 export function fromIterable<A>(iterable: Iterable<A>): Stream<A>;
-export function fromIterable(iterable: Iterable<any>): Stream<any>;
--- type-definitions/source/iterate.d.ts
+++ type-definitions/source/iterate.d.ts
@@ -1,4 +1,4 @@
 import { Stream } from '../types';
 
 export function iterate<A>(f: (a: A) => A | Promise<A>, a: A): Stream<A>;
-export function iterate<A>(f: (a: A) => A | Promise<A>, a: A): Stream<A>;
+export function iterate<A>(f: (a: A) => A | Promise<A>): (a: A) => Stream<A>;
