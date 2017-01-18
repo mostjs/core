5288e0e8c94cca72f2050a9cada7cacaf9efd807
Tylor Steinberger <tlsteinberger167@gmail.com>
2017-01-18 13:47:38 -0500

    chore(PropagateTask): update to new functional creation methods

--- type-definitions/PropagateTask.d.ts
+++ type-definitions/PropagateTask.d.ts
@@ -1,18 +1,15 @@
 import { Sink, Task } from './types';
 
-export class PropagateTask<T> implements Task {
-  protected _run: (time: number, value: T, sink: Sink<T>) => any;
-  protected value: T;
-  protected sink: Sink<T>;
-  protected active: boolean;
+export function propagateTask<T>(time: number, value: T, sink: Sink<T>): Task;
+export function propagateTask<T>(time: number, value: T): (sink: Sink<T>) => Task;
+export function propagateTask<T>(time: number): (value: T, sink: Sink<T>) => Task;
+export function propagateTask<T>(time: number): (value: T) => (sink: Sink<T>) => Task;
 
-  constructor(run: (time: number, value: T, sink: Sink<T>) => any, value: T, sink: Sink<T>);
+export function propagateEventTask<T>(value: T, sink: Sink<T>): Task;
+export function propagateEventTask<T>(value: T): (sink: Sink<T>) => Task;
 
-  public static event<T>(value: T, sink: Sink<T>): PropagateTask<T>;
-  public static error(error: Error, sink: Sink<any>): PropagateTask<any>;
-  public static end<T>(value: T, sink: Sink<T>): PropagateTask<T>;
+export function propagateEndTask<T>(value: T | void, sink: Sink<T>): Task;
+export function propagateEndTask<T>(value: T | void): (sink: Sink<T>) => Task;
 
-  public run(time: number): void;
-  public error(time: number, e: Error): void;
-  public dispose(): void;
-}
+export function propagateErrorTask(error: Error, sink: Sink<any>): Task;
+export function propagateErrorTask(error: Error): (sink: Sink<any>) => Task;
