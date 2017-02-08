02f7d84d901b4672a40bac66dee255ad9e9fbab2
Tylor Steinberger <tlsteinberger167@gmail.com>
2017-02-08 17:53:33 -0500

    chore(type-definitions): fix compiler errors (#40)

--- type-definitions/combinator/zip.d.ts
+++ type-definitions/combinator/zip.d.ts
@@ -4,7 +4,7 @@ export function zip<A, B, R>(fn: (a: A, b: B) => R, a: Stream<A>, b: Stream<B>):
 export function zip<A, B, C, R>(fn: (a: A, b: B, c: C) => R, a: Stream<A>, b: Stream<B>, c: Stream<C>): Stream<R>;
 export function zip<A, B, C, D, R>( fn: (a: A, b: B, c: C, d: D) => R, a: Stream<A>, b: Stream<B>, c: Stream<C>, d: Stream<D>): Stream<R>;
 export function zip<A, B, C, D, E, R>(fn: (a: A, b: B, c: C, d: D, e: E) => R, a: Stream<A>, b: Stream<B>, c: Stream<C>, d: Stream<D>, e: Stream<E>): Stream<R>;
-export function zip<R>(f: (...args: any[]) => R, ...streams: Stream<any>[]);
+export function zip<R>(f: (...args: any[]) => R, ...streams: Stream<any>[]): Stream<R>;
 
 export function zipArray<A, B, R>(
   fn: (a: A, b: B) => R,
