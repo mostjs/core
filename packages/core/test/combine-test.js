69105c217ff66a841bdc265d631c76767657149b
Brian Cavalier <brian@hovercraftstudios.com>
2017-02-12 17:31:23 -0500

    chore(test): port some top-level tests to mocha

---
+++ packages/core/test/combine-test.js
@@ -0,0 +1,70 @@
+import { describe, it } from 'mocha'
+import { eq } from '@briancavalier/assert'
+
+import { combine, combineArray } from '../src/combinator/combine'
+import { delay } from '../src/combinator/delay'
+import { just } from '../src/source/core'
+
+import { collectEventsFor, makeEvents, makeEventsFromArray } from '../test-buster-old/helper/testEnv'
+
+const sentinel = { value: 'sentinel' }
+
+describe('combine', function () {
+  it('should yield initial only after all inputs yield', function () {
+    const s1 = just(1)
+    const s2 = just(sentinel)
+
+    const sc = combine(Array, s1, delay(1, s2))
+
+    return collectEventsFor(2, sc)
+      .then(eq([{ time: 1, value: [1, sentinel] }]))
+  })
+
+  it('should yield when any input stream yields', function () {
+    const a = ['a', 'b', 'c', 'd']
+    const s1 = makeEvents(2, a.length)
+    const s2 = delay(1, makeEventsFromArray(2, a))
+
+    const sc = combine(Array, s1, s2)
+
+    return collectEventsFor(2 * a.length, sc)
+      .then(eq([
+        { time: 1, value: [ 0, 'a' ] },
+        { time: 2, value: [ 1, 'a' ] },
+        { time: 3, value: [ 1, 'b' ] },
+        { time: 4, value: [ 2, 'b' ] },
+        { time: 5, value: [ 2, 'c' ] },
+        { time: 6, value: [ 3, 'c' ] },
+        { time: 7, value: [ 3, 'd' ] }]))
+  })
+})
+
+describe('combineArray', function () {
+  it('should yield initial only after all inputs yield', function () {
+    const s1 = just(1)
+    const s2 = just(sentinel)
+
+    const sc = combineArray(Array, [s1, delay(1, s2)])
+
+    return collectEventsFor(2, sc)
+      .then(eq([{ time: 1, value: [1, sentinel] }]))
+  })
+
+  it('should yield when any input stream yields', function () {
+    const a = ['a', 'b', 'c', 'd']
+    const s1 = makeEvents(2, a.length)
+    const s2 = delay(1, makeEventsFromArray(2, a))
+
+    const sc = combineArray(Array, [s1, s2])
+
+    return collectEventsFor(2 * a.length, sc)
+      .then(eq([
+        { time: 1, value: [ 0, 'a' ] },
+        { time: 2, value: [ 1, 'a' ] },
+        { time: 3, value: [ 1, 'b' ] },
+        { time: 4, value: [ 2, 'b' ] },
+        { time: 5, value: [ 2, 'c' ] },
+        { time: 6, value: [ 3, 'c' ] },
+        { time: 7, value: [ 3, 'd' ] }]))
+  })
+})
---
+++ packages/core/test/filter-test.js
@@ -0,0 +1,54 @@
+import { describe, it } from 'mocha'
+import { eq } from '@briancavalier/assert'
+
+import { filter, skipRepeats, skipRepeatsWith } from '../src/combinator/filter'
+
+import { collectEventsFor, makeEventsFromArray } from '../test-buster-old/helper/testEnv'
+
+const sentinel = { value: 'sentinel' }
+const other = { value: 'other' }
+
+describe('filter', function () {
+  it('should return a stream containing only allowed items', function () {
+    const a = [sentinel, other, sentinel, other]
+    const p = x => x === sentinel
+    const s = filter(p, makeEventsFromArray(1, a))
+
+    return collectEventsFor(a.length, s)
+      .then(eq([
+        { time: 0, value: sentinel },
+        { time: 2, value: sentinel }
+      ]))
+  })
+})
+
+describe('skipRepeats', function () {
+  it('should return a stream with repeated events removed', function () {
+    const a = [1, 2, 2, 3, 4, 4]
+    const s = skipRepeats(makeEventsFromArray(1, a))
+
+    return collectEventsFor(a.length, s)
+      .then(eq([
+        { time: 0, value: 1 },
+        { time: 1, value: 2 },
+        { time: 3, value: 3 },
+        { time: 4, value: 4 }
+      ]))
+  })
+})
+
+describe('skipRepeatsWith', function () {
+  it('should use provided comparator to remove repeated events', function () {
+    const compare = (a, b) => a.toLowerCase() === b.toLowerCase()
+    const a = ['a', 'b', 'B', 'c', 'D', 'd']
+    const s = skipRepeatsWith(compare, makeEventsFromArray(1, a))
+
+    return collectEventsFor(a.length, s)
+      .then(eq([
+        { time: 0, value: 'a' },
+        { time: 1, value: 'b' },
+        { time: 3, value: 'c' },
+        { time: 4, value: 'D' }
+      ]))
+  })
+})
