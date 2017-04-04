6b31348f240a58548d5e6fbf1a8be5c071c9e0ac
Nissan Hanina <nissan.hanina@ca.com>
2017-04-04 14:56:43 +0300

    chore(type-definitions): fixed until curried type, rename skipUntil

--- packages/core/type-definitions/combinator/timeslice.d.ts
+++ packages/core/type-definitions/combinator/timeslice.d.ts
@@ -1,10 +1,10 @@
 import { Stream } from '@most/types';
 
 export function until<A>(signal: Stream<any>, s: Stream<A>): Stream<A>;
-export function until<A>(signal: Stream<A>): (s: Stream<A>) => Stream<A>;
+export function until<A>(signal: Stream<any>): (s: Stream<A>) => Stream<A>;
 
-export function skipUntil<A>(signal: Stream<any>, s: Stream<A>): Stream<A>;
-export function skipUntil<A>(signal: Stream<any>): (s: Stream<A>) => Stream<A>;
+export function since<A>(signal: Stream<any>, s: Stream<A>): Stream<A>;
+export function since<A>(signal: Stream<any>): (s: Stream<A>) => Stream<A>;
 
 export function during<A>(timeWindow: Stream<Stream<any>>, s: Stream<A>): Stream<A>;
 export function during<A>(timeWindow: Stream<Stream<any>>): (s: Stream<A>) => Stream<A>;
