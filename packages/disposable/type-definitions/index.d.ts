4c75ef1f54a4bae4e282b75457251e95507914cc
Tylor Steinberger <tlsteinberger167@gmail.com>
2017-05-03 20:47:34 -0400

    fix(types): Fix 2 errors in typescript definitions (#56)
    * chore(disposable): fix type errors
    
    * chore(core): fix type errors

--- packages/disposable/type-definitions/index.d.ts
+++ packages/disposable/type-definitions/index.d.ts
@@ -1,7 +1,7 @@
 import { Time, Disposable, Sink } from '@most/types';
 
 declare function disposeNone (): Disposable;
-declare function disposeWith <R> (dispose: (R) => void, resource: R): Disposable;
+declare function disposeWith <R> (dispose: (resource: R) => void, resource: R): Disposable;
 declare function disposeOnce (d: Disposable): Disposable;
 declare function disposeBoth (d1: Disposable, d2: Disposable): Disposable;
 declare function disposeAll (ds: Array<Disposable>): Disposable;
