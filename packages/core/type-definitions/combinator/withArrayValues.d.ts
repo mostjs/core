0d17b1b6909f12169c1b5d5e56a053bb8e7b3b5e
Nissan Hanina <nissan@yo.ma>
2017-05-14 16:26:04 +0300

    fix(types): missing withArrayValues

--- packages/core/type-definitions/combinator/withArrayValues.d.ts
+++ packages/core/type-definitions/combinator/withArrayValues.d.ts
@@ -4,3 +4,6 @@ export function zipArrayValues <A, B, C> (f: (a: A, b: B) => C, a: Array<A>, s: 
 export function zipArrayValues <A, B, C> (f: (a: A, b: B) => C): (a: Array<A>, s: Stream<B>) => Stream<C>;
 export function zipArrayValues <A, B, C> (f: (a: A, b: B) => C, a: Array<A>): (s: Stream<B>) => Stream<C>;
 export function zipArrayValues <A, B, C> (f: (a: A, b: B) => C): (a: Array<A>) => (s: Stream<B>) => Stream<C>;
+
+export function withArrayValues <A> (a: Array<A>, s: Stream<any>): Stream<A>
+export function withArrayValues <A> (a: Array<A>): (s: Stream<any>) => Stream<A> 
