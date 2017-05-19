e7b9ec645339b42bacd17aebb7ec5657b9b1b426
Brian Cavalier <brian@briancavalier.com>
2017-05-19 18:05:41 -0400

    feat(virtual-stream): self-contained fromStream() that doesn't use runEffects

--- packages/virtual-stream/src/fromStream.js
+++ packages/virtual-stream/src/fromStream.js
@@ -1,21 +1,24 @@
 import { at } from './event'
 import { finite, never, errored } from './vstream'
-import { runEffects } from '@most/core'
 
-export const fromStream = (stream, scheduler) => {
-  const collect = new Collect(stream)
-  return runEffects(collect, scheduler).then(() => collect.vstream)
+export const fromStream = (stream, scheduler) =>
+  new Promise(collect(stream, scheduler))
+    .then(disposeAndReturn)
+
+const collect = (stream, scheduler) => resolve =>
+  new Collect(stream, scheduler, resolve)
+
+const disposeAndReturn = ({ disposable, vstream }) => {
+  disposable.dispose()
+  return vstream
 }
 
 class Collect {
-  constructor (stream) {
-    this.stream = stream
+  constructor (stream, scheduler, resolve) {
+    this.resolve = resolve
     this.events = []
     this.vstream = never()
-  }
-
-  run (sink, scheduler) {
-    return this.stream.run(new CollectSink(sink, this), scheduler)
+    this.disposable = stream.run(this, scheduler)
   }
 
   event (t, x) {
@@ -24,31 +27,11 @@ class Collect {
 
   end (t) {
     this.vstream = finite(this.events, t)
+    this.resolve(this)
   }
 
   error (t, e) {
     this.vstream = errored(e, this.events, t)
-  }
-}
-
-class CollectSink {
-  constructor (sink1, sink2) {
-    this.sink1 = sink1
-    this.sink2 = sink2
-  }
-
-  event (t, x) {
-    this.sink1.event(t, x)
-    this.sink2.event(t, x)
-  }
-
-  end (t) {
-    this.sink1.end(t)
-    this.sink2.end(t)
-  }
-
-  error (t, e) {
-    this.sink1.event(t, e)
-    this.sink2.event(t, e)
+    this.resolve(this)
   }
 }
