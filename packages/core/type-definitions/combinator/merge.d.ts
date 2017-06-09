7a4b38966377ac6d61583d5d90f6485d9905f1e5
joshburgess <joshburgess.webdev@gmail.com>
2017-06-08 23:21:37 -0400

    Add curried type definition variants for merge

--- packages/core/type-definitions/combinator/merge.d.ts
+++ packages/core/type-definitions/combinator/merge.d.ts
@@ -1,4 +1,5 @@
 import { Stream } from '@most/types';
 
-export function merge<A>(a: Stream<A>, b: Stream<A>): Stream<A>;
+export function merge<A>(s1: Stream<A>, s2: Stream<A>): Stream<A>;
+export function merge<A>(s1: Stream<A>): (s2: Stream<A>) => Stream<A>
 export function mergeArray<A>(streams: Array<Stream<A>>): Stream<A>;
