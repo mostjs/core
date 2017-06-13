0f75d09dd171fea46695369a07fa786acc2e3b6e
Brian Cavalier <brian@briancavalier.com>
2017-06-12 21:40:53 -0400

    chore(unfold): Remove unfold, iterate, generate

--- packages/core/type-definitions/source/unfold.d.ts
+++
@@ -1,5 +0,0 @@
-import { Stream } from '@most/types';
-import { SeedValue } from '../seedValue';
-
-export function unfold<A, B, S>(f: (seed: S) => SeedValue<S, B|Promise<B>>, seed: S): Stream<B>;
-export function unfold<A, B, S>(f: (seed: S) => SeedValue<S, B|Promise<B>>): (seed: S) => Stream<B>;
