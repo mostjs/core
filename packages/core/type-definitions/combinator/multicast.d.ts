9d16e368703881197c0b1b401962648a127ea26a
Tylor Steinberger <tlsteinberger167@gmail.com>
2017-07-13 07:58:12 -0400

    feat(type-definitions): add MulticastSource typings

--- packages/core/type-definitions/combinator/multicast.d.ts
+++ packages/core/type-definitions/combinator/multicast.d.ts
@@ -1,3 +1,20 @@
-import { Stream } from '@most/types';
+import { Disposable, Scheduler, Sink, Stream, Time } from '@most/types';
 
 export function multicast<A>(s: Stream<A>): Stream<A>;
+
+export class MulticastSource<A> implements Stream<A>, Sink<A> {
+  protected source: Stream<A>;
+  protected sinks: Array<Sink<A>>;
+  protected _disposable: Disposable;
+
+  constructor(source: Stream<A>);
+
+  public run(sink: Sink<A>, scheduler: Scheduler): Disposable;
+
+  public event(time: Time, value: A): void;
+  public error(time: Time, error: Error): void;
+  public end(time: Time): void;
+
+  protected add(sink: Sink<A>): number;
+  protected remove(sink: Sink<A>): number;
+}
