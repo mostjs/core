1f9d788ef93bc2ebddec532bc79c50dddcdd30ca
Brian Cavalier <brian@briancavalier.com>
2017-08-24 09:43:56 -0400

    feat(disposable): curry API (#110)

--- packages/disposable/type-definitions/index.d.ts
+++ packages/disposable/type-definitions/index.d.ts
@@ -1,9 +1,18 @@
 import { Time, Disposable, Sink } from '@most/types';
 
 declare function disposeNone (): Disposable;
+
 declare function disposeWith <R> (dispose: (resource: R) => void, resource: R): Disposable;
+declare function disposeWith <R> (dispose: (resource: R) => void): (resource: R) => Disposable
+
 declare function disposeOnce (d: Disposable): Disposable;
+
 declare function disposeBoth (d1: Disposable, d2: Disposable): Disposable;
+declare function disposeBoth (d1: Disposable): (d2: Disposable) => Disposable
+
 declare function disposeAll (ds: Array<Disposable>): Disposable;
 
-declare function tryDispose (t: Time, disposable: Disposable, sink: Sink<any>): void;
+declare function tryDispose (t: Time, disposable: Disposable, sink: Sink<any>): void
+declare function tryDispose (t: Time): (disposable: Disposable, sink: Sink<any>) => void
+declare function tryDispose (t: Time, disposable: Disposable): (sink: Sink<any>) => void
+declare function tryDispose (t: Time): (disposable: Disposable) => (sink: Sink<any>) => void
