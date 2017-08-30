d432d75963e05d2645203fcd54acf4f931ea8da8
Brian Cavalier <brian@briancavalier.com>
2017-08-29 21:21:46 -0400

    fix(dispose): Export dispose

--- packages/disposable/src/index.js
+++ packages/disposable/src/index.js
@@ -1,5 +1,6 @@
 /** @license MIT License (c) copyright 2010-2017 original author or authors */
 
+export { dispose } from './dispose'
 export { disposeNone } from './disposeNone'
 export { disposeWith } from './disposeWith'
 export { disposeOnce } from './disposeOnce'
