94539faad5d3488b8d5c1bdbeed356d275a3c7d6
Brian Cavalier <brian@briancavalier.com>
2017-08-29 21:17:10 -0400

    feat(disposable): Add dispose function, test, api docs. Fix types

---
+++ packages/disposable/src/dispose.js
@@ -0,0 +1,4 @@
+/** @license MIT License (c) copyright 2010-2017 original author or authors */
+
+export const dispose = disposable =>
+  disposable.dispose()
--- packages/disposable/type-definitions/index.d.ts
+++ packages/disposable/type-definitions/index.d.ts
@@ -1,18 +1,20 @@
 import { Time, Disposable, Sink } from '@most/types';
 
-declare function disposeNone (): Disposable;
+export function disposeNone (): Disposable;
 
-declare function disposeWith <R> (dispose: (resource: R) => void, resource: R): Disposable;
-declare function disposeWith <R> (dispose: (resource: R) => void): (resource: R) => Disposable
+export function disposeWith <R> (dispose: (resource: R) => void, resource: R): Disposable;
+export function disposeWith <R> (dispose: (resource: R) => void): (resource: R) => Disposable
 
-declare function disposeOnce (d: Disposable): Disposable;
+export function disposeOnce (d: Disposable): Disposable;
 
-declare function disposeBoth (d1: Disposable, d2: Disposable): Disposable;
-declare function disposeBoth (d1: Disposable): (d2: Disposable) => Disposable
+export function disposeBoth (d1: Disposable, d2: Disposable): Disposable;
+export function disposeBoth (d1: Disposable): (d2: Disposable) => Disposable
 
-declare function disposeAll (ds: Array<Disposable>): Disposable;
+export function disposeAll (ds: Array<Disposable>): Disposable;
 
-declare function tryDispose (t: Time, disposable: Disposable, sink: Sink<any>): void
-declare function tryDispose (t: Time): (disposable: Disposable, sink: Sink<any>) => void
-declare function tryDispose (t: Time, disposable: Disposable): (sink: Sink<any>) => void
-declare function tryDispose (t: Time): (disposable: Disposable) => (sink: Sink<any>) => void
+export function dispose (d: Disposable): void
+
+export function tryDispose (t: Time, disposable: Disposable, sink: Sink<any>): void
+export function tryDispose (t: Time): (disposable: Disposable, sink: Sink<any>) => void
+export function tryDispose (t: Time, disposable: Disposable): (sink: Sink<any>) => void
+export function tryDispose (t: Time): (disposable: Disposable) => (sink: Sink<any>) => void
