4e0daa3d656d2864a7278d69189dac71c218fa00
Nathan Ridley <axefrog@gmail.com>
2017-09-02 11:35:49 +1000

    fix(types): propagateTask; decouple value type from sink type

--- packages/core/type-definitions/PropagateTask.d.ts
+++ packages/core/type-definitions/PropagateTask.d.ts
@@ -1,9 +1,9 @@
 import { Sink, Task } from '@most/types';
 
-export function propagateTask<T>(run: PropagateTaskRun<T>, value: T, sink: Sink<T>): PropagateTask<T>;
-export function propagateTask<T>(run: PropagateTaskRun<T>, value: T): (sink: Sink<T>) => PropagateTask<T>;
-export function propagateTask<T>(run: PropagateTaskRun<T>): (value: T, sink: Sink<T>) => PropagateTask<T>;
-export function propagateTask<T>(run: PropagateTaskRun<T>): (value: T) => (sink: Sink<T>) => PropagateTask<T>;
+export function propagateTask<A, B = A>(run: PropagateTaskRun<A, B>, value: A, sink: Sink<B>): PropagateTask<A, B>;
+export function propagateTask<A, B = A>(run: PropagateTaskRun<A, B>, value: A): (sink: Sink<B>) => PropagateTask<A, B>;
+export function propagateTask<A, B = A>(run: PropagateTaskRun<A, B>): (value: A, sink: Sink<B>) => PropagateTask<A, B>;
+export function propagateTask<A, B = A>(run: PropagateTaskRun<A, B>): (value: A) => (sink: Sink<B>) => PropagateTask<A, B>;
 
 export function propagateEventTask<T>(value: T, sink: Sink<T>): PropagateTask<T>;
 export function propagateEventTask<T>(value: T): (sink: Sink<T>) => PropagateTask<T>;
@@ -13,11 +13,11 @@ export function propagateEndTask<T>(sink: Sink<T>): PropagateTask<void>;
 export function propagateErrorTask(error: Error, sink: Sink<any>): PropagateTask<any>;
 export function propagateErrorTask(error: Error): (sink: Sink<any>) => PropagateTask<any>;
 
-export type PropagateTaskRun<A> =
-  (time: number, value: A, sink: Sink<A>) => any
+export type PropagateTaskRun<A, B = A> =
+  (time: number, value: A, sink: Sink<B>) => any
 
-export interface PropagateTask<A> extends Task {
+export interface PropagateTask<A, B = A> extends Task {
   value: A;
-  sink: Sink<A>;
+  sink: Sink<B>;
   active: boolean;
 }
