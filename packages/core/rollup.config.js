aa86d3302fd564d0d8ed82b0d13368432dbe4657
Brian Cavalier <brian@briancavalier.com>
2017-10-06 20:13:27 -0400

    chore(build): stop bundling deps into builds (#141)
    * chore(build): stop bundling deps into builds
    
    * chore(build): remove now-unused rollup-plugin-node-resolve
    
    * chore(build): add external to rollup configs

--- packages/core/rollup.config.js
+++ packages/core/rollup.config.js
@@ -1,20 +1,26 @@
 import buble from 'rollup-plugin-buble'
-import nodeResolve from 'rollup-plugin-node-resolve'
 
 export default {
   entry: 'src/index.js',
   plugins: [
-    buble(),
-    nodeResolve({
-      jsnext: true
-    })
+    buble()
+  ],
+  external: [
+    '@most/scheduler',
+    '@most/disposable',
+    '@most/prelude'
   ],
   targets: [
     {
       dest: 'dist/mostCore.js',
       format: 'umd',
       moduleName: 'mostCore',
-      sourceMap: true
+      sourceMap: true,
+      globals: {
+        '@most/scheduler': 'mostScheduler',
+        '@most/disposable': 'mostDisposable',
+        '@most/prelude': 'mostPrelude'
+      }
     },
     {
       dest: 'dist/mostCore.es.js',
