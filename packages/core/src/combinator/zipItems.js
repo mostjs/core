7791979875bbc71c13804a5197aa9cb8b9620b48
Tylor Steinberger <tlsteinberger167@gmail.com>
2017-10-13 08:21:59 -0400

    fix: simplify zipItems logic (#149)
    * fix: simplify ZipArrayValuesSink logic
    
    Use take(array.length) to remove duplicate logic in ZipArrayValuesSink
    
    * chore(zipItems): rebase
    
    * chore(zipItems): add regression test based on https://www.webpackbin.com/bins/-KvrqudMj6C-0H494Itg
    
    * feat: use new empty optimizations
    
    Only check isCanonicalEmpty in zipItems and rely on take(0) returning empty()
    
    * feat: ensure empty() optimizations are propagated
    
    * chore(zipItems): refactor empty check

--- packages/core/src/combinator/zipItems.js
+++ packages/core/src/combinator/zipItems.js
@@ -1,15 +1,16 @@
 /** @license MIT License (c) copyright 2017 original author or authors */
 
-import { empty } from '../source/empty'
 import Pipe from '../sink/Pipe'
+import { empty, isCanonicalEmpty } from '../source/empty'
+import { take } from './slice'
 
 export const withItems = (items, stream) =>
   zipItems(keepLeft, items, stream)
 
 export const zipItems = (f, items, stream) =>
-  items.length === 0 || stream === empty()
+  isCanonicalEmpty(stream) || items.length === 0
     ? empty()
-    : new ZipItems(f, items, stream)
+    : new ZipItems(f, items, take(items.length, stream))
 
 const keepLeft = (a, _) => a
 
@@ -36,10 +37,6 @@ class ZipItemsSink extends Pipe {
   event (t, b) {
     const f = this.f
     this.sink.event(t, f(this.items[this.index], b))
-
     this.index += 1
-    if (this.index >= this.items.length) {
-      this.sink.end(t)
-    }
   }
 }
