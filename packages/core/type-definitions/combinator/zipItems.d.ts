a1f792b0f7bd21b8b43b97e782c3fda5a7802f06
Brian Cavalier <brian@briancavalier.com>
2017-10-17 07:22:54 -0400

    fix(types): Fix zipItems/withItems types

--- packages/core/type-definitions/combinator/zipItems.d.ts
+++ packages/core/type-definitions/combinator/zipItems.d.ts
@@ -1,9 +1,9 @@
 import { Stream } from '@most/types'
 
-export function zipArrayValues <A, B, C> (f: (a: A, b: B) => C, a: Array<A>, s: Stream<B>): Stream<C>;
-export function zipArrayValues <A, B, C> (f: (a: A, b: B) => C): (a: Array<A>, s: Stream<B>) => Stream<C>;
-export function zipArrayValues <A, B, C> (f: (a: A, b: B) => C, a: Array<A>): (s: Stream<B>) => Stream<C>;
-export function zipArrayValues <A, B, C> (f: (a: A, b: B) => C): (a: Array<A>) => (s: Stream<B>) => Stream<C>;
+export function zipItems <A, B, C> (f: (a: A, b: B) => C, a: Array<A>, s: Stream<B>): Stream<C>;
+export function zipItems <A, B, C> (f: (a: A, b: B) => C): (a: Array<A>, s: Stream<B>) => Stream<C>;
+export function zipItems <A, B, C> (f: (a: A, b: B) => C, a: Array<A>): (s: Stream<B>) => Stream<C>;
+export function zipItems <A, B, C> (f: (a: A, b: B) => C): (a: Array<A>) => (s: Stream<B>) => Stream<C>;
 
-export function withArrayValues <A> (a: Array<A>, s: Stream<any>): Stream<A>
-export function withArrayValues <A> (a: Array<A>): (s: Stream<any>) => Stream<A> 
+export function withItems <A> (a: Array<A>, s: Stream<any>): Stream<A>
+export function withItems <A> (a: Array<A>): (s: Stream<any>) => Stream<A>
