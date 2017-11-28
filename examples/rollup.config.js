6ab4da31026240827bf8a59749e1296c6305ba84
Brian Cavalier <brian@briancavalier.com>
2017-11-28 08:09:14 -0500

    Add uglify to test PURE annotation tree-shaking

--- examples/rollup.config.js
+++ examples/rollup.config.js
@@ -1,10 +1,13 @@
 import node from 'rollup-plugin-node-resolve'
 import flow from 'rollup-plugin-flow'
+import uglify from 'rollup-plugin-uglify'
+import { minify } from 'uglify-es'
 
 export default {
   plugins: [
     flow(),
-    node()
+    node(),
+    uglify({}, minify)
   ],
   output: {
     format: 'iife',
