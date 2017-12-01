f8b428c05a6de8b53c7a47345fdc08a013ee691b
Brian Cavalier <brian@briancavalier.com>
2017-11-30 20:49:00 -0500

    Add containsCanonicalEmpty

--- packages/core/test/source/empty-test.js
+++ packages/core/test/source/empty-test.js
@@ -2,7 +2,8 @@
 import { describe, it } from 'mocha'
 import { eq, assert } from '@briancavalier/assert'
 
-import { empty, isCanonicalEmpty } from '../../src/source/empty'
+import { empty, isCanonicalEmpty, containsCanonicalEmpty } from '../../src/source/empty'
+import { now } from '../../src/source/now'
 
 import { collectEventsFor, makeEvents } from '../helper/testEnv'
 
@@ -14,6 +15,20 @@ describe('empty', () => {
 
     it('given non-empty(), should return false', () => {
       assert(!isCanonicalEmpty(makeEvents(1, 0)))
+    })
+  })
+
+  describe('containsCanonicalEmpty', () => {
+    it('given empty array, should return false', () => {
+      assert(!containsCanonicalEmpty([]))
+    })
+
+    it('given array containing  empty(), should return true', () => {
+      assert(containsCanonicalEmpty([now(0), empty(), now(1)]))
+    })
+
+    it('given array containing only non-empty(), should return false', () => {
+      assert(!containsCanonicalEmpty([now(0), now(1)]))
     })
   })
 
