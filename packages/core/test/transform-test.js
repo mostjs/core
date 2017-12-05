a4caa50fd0162e5db2bc15ff504d27ee0aa4670f
Frederik Krautwald <fkrautwald@gmail.com>
2017-12-05 23:08:10 +0100

    Map return canonical empty (#179)
    * Return same canonical empty stream
    
    Close #178
    
    Signed-off-by: Frederik Krautwald <fkrautwald@gmail.com>
    
    * Use isCanonicalEmpty in test
    
    Signed-off-by: Frederik Krautwald <fkrautwald@gmail.com>
    
    * Correct test description
    
    Signed-off-by: Frederik Krautwald <fkrautwald@gmail.com>
    
    * Return empty() when source is canonical empty
    
    Adhere to signature:
    
    map :: (a -> b) -> Stream a -> Stream b
    
    Signed-off-by: Frederik Krautwald <fkrautwald@gmail.com>
    
    * Use id from prelude
    
    Signed-off-by: Frederik Krautwald <fkrautwald@gmail.com>

--- packages/core/test/transform-test.js
+++ packages/core/test/transform-test.js
@@ -1,11 +1,13 @@
 import { describe, it } from 'mocha'
-import { eq } from '@briancavalier/assert'
+import { eq, assert } from '@briancavalier/assert'
 
 import { assertSame } from './helper/stream-helper'
 import { map, tap, constant } from '../src/combinator/transform'
 import { now } from '../src/source/now'
+import { empty, isCanonicalEmpty } from '../src/source/empty'
 
 import { collectEventsFor } from './helper/testEnv'
+import { id } from '@most/prelude'
 
 describe('map', function () {
   it('should satisfy identity', function () {
@@ -25,6 +27,17 @@ describe('map', function () {
       map(x => f(g(x)), u),
       map(f, map(g, u))
     )
+  })
+
+  describe('given a canonical empty stream', function () {
+    it('should return a canonical empty stream', function () {
+      // Fixture setup
+      const emptyStream = empty()
+      // Exercise system
+      const sut = map(id, emptyStream)
+      // Verify outcome
+      assert(isCanonicalEmpty(sut))
+    })
   })
 })
 
