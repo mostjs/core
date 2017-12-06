5a83f32780a45a1f078a5c1d138f69a032db0db9
Frederik Krautwald <fkrautwald@gmail.com>
2017-12-06 22:20:53 +0100

    Remove redundant canonical empty check
    combine() already performs this check that makes test pass.
    
    Signed-off-by: Frederik Krautwald <fkrautwald@gmail.com>

--- packages/core/src/combinator/applicative.js
+++ packages/core/src/combinator/applicative.js
@@ -4,7 +4,6 @@
 
 import { combine } from './combine'
 import { apply } from '@most/prelude'
-import { containsCanonicalEmpty, empty } from '../source/empty'
 
 /**
  * Assume fs is a stream containing functions, and apply the latest function
@@ -17,9 +16,5 @@ import { containsCanonicalEmpty, empty } from '../source/empty'
  * @returns {Stream} stream containing all the applications of fs to xs
  */
 export function ap (fs, xs) {
-  if (containsCanonicalEmpty([fs, xs])) {
-    return empty()
-  }
-
   return combine(apply, fs, xs)
 }
