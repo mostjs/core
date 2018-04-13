1fda38ddfb4de7b15669cb7f9d7421e5f530cecc
Nikos Kalogridis <nkalogridis@tripsta.com>
2018-04-13 16:39:43 +0300

    fix standard errors previously hidden by eslint directive

--- packages/core/src/index.js
+++ packages/core/src/index.js
@@ -1,5 +1,34 @@
 /** @license MIT License (c) copyright 2016 original author or authors */
 import { curry2, curry3 } from '@most/prelude'
+import { zipItems as _zipItems, withItems as _withItems } from './combinator/zipItems'
+import { runEffects as _runEffects } from './runEffects'
+import { run as _run } from './run'
+import { withLocalTime as _withLocalTime } from './combinator/withLocalTime'
+import { loop as _loop } from './combinator/loop'
+import { scan as _scan } from './combinator/scan'
+import { startWith as _startWith } from './combinator/startWith'
+import { map as _map, constant as _constant, tap as _tap } from './combinator/transform'
+import { ap as _ap } from './combinator/applicative'
+import { chain as _chain, join } from './combinator/chain'
+import { continueWith as _continueWith } from './combinator/continueWith'
+import { concatMap as _concatMap } from './combinator/concatMap'
+import { mergeConcurrently as _mergeConcurrently, mergeMapConcurrently as _mergeMapConcurrently } from './combinator/mergeConcurrently'
+import { merge as _merge, mergeArray } from './combinator/merge'
+import { combine as _combine, combineArray as _combineArray } from './combinator/combine'
+import { sample as _sample, snapshot as _snapshot } from './combinator/snapshot'
+import { zip as _zip, zipArray as _zipArray } from './combinator/zip'
+import { filter as _filter, skipRepeats, skipRepeatsWith as _skipRepeatsWith } from './combinator/filter'
+import { take as _take, skip as _skip, slice as _slice, takeWhile as _takeWhile, skipWhile as _skipWhile, skipAfter as _skipAfter } from './combinator/slice'
+import { until as _until, since as _since, during as _during } from './combinator/timeslice'
+import { delay as _delay } from './combinator/delay'
+import { throttle as _throttle, debounce as _debounce } from './combinator/limit'
+import { recoverWith as _recoverWith, throwError } from './combinator/errors'
+import {
+  propagateTask as _propagateTask,
+  propagateEventTask as _propagateEventTask,
+  propagateErrorTask as _propagateErrorTask,
+  propagateEndTask
+} from './scheduler/PropagateTask'
 
 export { empty } from './source/empty'
 export { never } from './source/never'
@@ -10,49 +39,34 @@ export { periodic } from './source/periodic'
 
 export { newStream } from './source/newStream'
 
-import { zipItems as _zipItems, withItems as _withItems } from './combinator/zipItems'
 export const zipItems = curry3(_zipItems)
 export const withItems = curry2(_withItems)
 
 // -----------------------------------------------------------------------
 // Observing
 
-import { runEffects as _runEffects } from './runEffects'
-import { run as _run } from './run'
-
 export const runEffects = curry2(_runEffects)
 export const run = curry3(_run)
 
 // -------------------------------------------------------
 
-import { withLocalTime as _withLocalTime } from './combinator/withLocalTime'
-
 export const withLocalTime = curry2(_withLocalTime)
 
 // -------------------------------------------------------
 
-import { loop as _loop } from './combinator/loop'
-
 export const loop = curry3(_loop)
 
 // -------------------------------------------------------
-
-import { scan as _scan } from './combinator/scan'
 
 export const scan = curry3(_scan)
 
 // -----------------------------------------------------------------------
 // Extending
 
-import { startWith as _startWith } from './combinator/startWith'
-
 export const startWith = curry2(_startWith)
 
 // -----------------------------------------------------------------------
 // Transforming
-
-import { map as _map, constant as _constant, tap as _tap } from './combinator/transform'
-import { ap as _ap } from './combinator/applicative'
 
 export const map = curry2(_map)
 export const constant = curry2(_constant)
@@ -62,20 +76,14 @@ export const ap = curry2(_ap)
 // -----------------------------------------------------------------------
 // FlatMapping
 
-import { chain as _chain, join } from './combinator/chain'
 export const chain = curry2(_chain)
 export { join }
 
-import { continueWith as _continueWith } from './combinator/continueWith'
 export const continueWith = curry2(_continueWith)
-
-import { concatMap as _concatMap } from './combinator/concatMap'
 export const concatMap = curry2(_concatMap)
 
 // -----------------------------------------------------------------------
 // Concurrent merging
-
-import { mergeConcurrently as _mergeConcurrently, mergeMapConcurrently as _mergeMapConcurrently } from './combinator/mergeConcurrently'
 
 export const mergeConcurrently = curry2(_mergeConcurrently)
 export const mergeMapConcurrently = curry3(_mergeMapConcurrently)
@@ -83,15 +91,11 @@ export const mergeMapConcurrently = curry3(_mergeMapConcurrently)
 // -----------------------------------------------------------------------
 // Merging
 
-import { merge as _merge, mergeArray } from './combinator/merge'
-
 export const merge = curry2(_merge)
 export { mergeArray }
 
 // -----------------------------------------------------------------------
 // Combining
-
-import { combine as _combine, combineArray as _combineArray } from './combinator/combine'
 
 export const combine = curry3(_combine)
 export const combineArray = curry2(_combineArray)
@@ -99,15 +103,11 @@ export const combineArray = curry2(_combineArray)
 // -----------------------------------------------------------------------
 // Sampling
 
-import { sample as _sample, snapshot as _snapshot } from './combinator/snapshot'
-
 export const sample = curry2(_sample)
 export const snapshot = curry3(_snapshot)
 
 // -----------------------------------------------------------------------
 // Zipping
-
-import { zip as _zip, zipArray as _zipArray } from './combinator/zip'
 
 export const zip = curry3(_zip)
 export const zipArray = curry2(_zipArray)
@@ -120,16 +120,12 @@ export { switchLatest } from './combinator/switch'
 // -----------------------------------------------------------------------
 // Filtering
 
-import { filter as _filter, skipRepeats, skipRepeatsWith as _skipRepeatsWith } from './combinator/filter'
-
 export const filter = curry2(_filter)
 export { skipRepeats }
 export const skipRepeatsWith = curry2(_skipRepeatsWith)
 
 // -----------------------------------------------------------------------
 // Slicing
-
-import { take as _take, skip as _skip, slice as _slice, takeWhile as _takeWhile, skipWhile as _skipWhile, skipAfter as _skipAfter } from './combinator/slice'
 
 export const take = curry2(_take)
 export const skip = curry2(_skip)
@@ -141,8 +137,6 @@ export const skipAfter = curry2(_skipAfter)
 // -----------------------------------------------------------------------
 // Time slicing
 
-import { until as _until, since as _since, during as _during } from './combinator/timeslice'
-
 export const until = curry2(_until)
 export const since = curry2(_since)
 export const during = curry2(_during)
@@ -150,14 +144,10 @@ export const during = curry2(_during)
 // -----------------------------------------------------------------------
 // Delaying
 
-import { delay as _delay } from './combinator/delay'
-
 export const delay = curry2(_delay)
 
 // -----------------------------------------------------------------------
 // Rate limiting
-
-import { throttle as _throttle, debounce as _debounce } from './combinator/limit'
 
 export const throttle = curry2(_throttle)
 export const debounce = curry2(_debounce)
@@ -170,8 +160,6 @@ export { fromPromise, awaitPromises } from './combinator/promises'
 // -----------------------------------------------------------------------
 // Error handling
 
-import { recoverWith as _recoverWith, throwError } from './combinator/errors'
-
 export const recoverWith = curry2(_recoverWith)
 export { throwError }
 
@@ -181,13 +169,6 @@ export { throwError }
 export { multicast, MulticastSource } from './combinator/multicast'
 
 // ----------------------------------------------------------------------
-import {
-  propagateTask as _propagateTask,
-  propagateEventTask as _propagateEventTask,
-  propagateErrorTask as _propagateErrorTask,
-  propagateEndTask
-} from './scheduler/PropagateTask'
-
 export const propagateTask = curry3(_propagateTask)
 export const propagateEventTask = curry2(_propagateEventTask)
 export const propagateErrorTask = curry2(_propagateErrorTask)
