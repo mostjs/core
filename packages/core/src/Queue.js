699109f4cc736f36cfb73527c8fcbaff9f5ab2f8
Brian Cavalier <brian@briancavalier.com>
2018-05-03 20:36:05 -0400

    Extract constants, add docs, simplify copyArray

--- packages/core/src/Queue.js
+++ packages/core/src/Queue.js
@@ -1,6 +1,18 @@
-/** @license MIT License (c) copyright 2010 original author or authors */
+// Copied and modified from https://github.com/invertase/denque
+// MIT License
 
-// Based on https://github.com/invertase/denque
+// These constants were extracted directly from denque's shift()
+// It's not clear exactly why the authors chose these particular
+// values, but given that denque's goals, it seems likely that
+// they were chosen for speed/memory reasons.
+
+// Max value of _head at which Queue is willing to shink
+// its internal array
+const HEAD_MAX_SHRINK = 2
+
+// Min value of _tail at which Queue is willing to shink
+// its internal array
+const TAIL_MIN_SHRINK = 10000
 
 export default class Queue {
   constructor () {
@@ -34,7 +46,9 @@ export default class Queue {
     const x = this._list[head]
     this._list[head] = undefined
     this._head = (head + 1) & this._capacityMask
-    if (head < 2 && this._tail > 10000 && this._tail <= this._list.length >>> 2) {
+    if (head < HEAD_MAX_SHRINK &&
+      this._tail > TAIL_MIN_SHRINK &&
+      this._tail <= this._list.length >>> 2) {
       this._shrinkArray()
     }
 
@@ -58,7 +72,7 @@ export default class Queue {
   _growArray () {
     if (this._head) {
       // copy existing data, head to end, then beginning to tail.
-      this._list = this._copyArray(true)
+      this._list = this._copyArray()
       this._head = 0
     }
 
@@ -74,23 +88,17 @@ export default class Queue {
     this._capacityMask >>>= 1
   }
 
-  _copyArray (fullCopy) {
+  _copyArray () {
     const newArray = []
     const list = this._list
     const len = list.length
 
     let i
-    if (fullCopy || this._head > this._tail) {
-      for (i = this._head; i < len; i++) {
-        newArray.push(list[i])
-      }
-      for (i = 0; i < this._tail; i++) {
-        newArray.push(list[i])
-      }
-    } else {
-      for (i = this._head; i < this._tail; i++) {
-        newArray.push(list[i])
-      }
+    for (i = this._head; i < len; i++) {
+      newArray.push(list[i])
+    }
+    for (i = 0; i < this._tail; i++) {
+      newArray.push(list[i])
     }
 
     return newArray
