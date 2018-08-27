df857f022244bdb80709d60c63f4d088c53d3110
Brian Cavalier <brian@briancavalier.com>
2018-08-27 07:06:11 -0400

    Remove src files, remove unneeded standard config

--- packages/most/src/index.d.ts
+++
@@ -1,4 +0,0 @@
-export * from '@most/types'
-export * from '@most/disposable'
-export * from '@most/scheduler'
-export * from '@most/core'
--- packages/most/src/index.js
+++
@@ -1,3 +0,0 @@
-export * from '@most/disposable'
-export * from '@most/scheduler'
-export * from '@most/core'
