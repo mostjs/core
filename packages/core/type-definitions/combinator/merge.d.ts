ea4f84824074660d499d456bd32338ab6b508caa
Brian Cavalier <brian@briancavalier.com>
2019-05-05 20:13:20 -0400

    Apply new technique for strong heterogenous array types

--- packages/core/type-definitions/combinator/merge.d.ts
+++ packages/core/type-definitions/combinator/merge.d.ts
@@ -2,4 +2,8 @@ import { Stream } from '@most/types';
 
 export function merge<A, B>(s1: Stream<A>, s2: Stream<B>): Stream<A | B>;
 export function merge<A, B>(s1: Stream<A>): (s2: Stream<B>) => Stream<A | B>
-export function mergeArray<A>(streams: Array<Stream<A>>): Stream<A>;
+
+type MergeArray<S extends ReadonlyArray<Stream<any>>> = Value<S[number]>
+type Value<S> = S extends Stream<infer A> ? A : never
+
+export function mergeArray<S extends ReadonlyArray<Stream<any>>>(streams: S): Stream<MergeArray<S>>;
