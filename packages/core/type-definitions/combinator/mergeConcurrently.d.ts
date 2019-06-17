78ae89cfe1a234c1635752154964b34b49a366bb
gitusp <ususso@gmail.com>
2019-06-17 20:07:04 +0900

    Add `mergeMapConcurrently` definition

--- packages/core/type-definitions/combinator/mergeConcurrently.d.ts
+++ packages/core/type-definitions/combinator/mergeConcurrently.d.ts
@@ -2,3 +2,8 @@ import { Stream } from '@most/types';
 
 export function mergeConcurrently<A>(concurrency: number, s: Stream<Stream<A>>): Stream<A>;
 export function mergeConcurrently<A>(concurrency: number): (s: Stream<Stream<A>>) => Stream<A>;
+
+export function mergeMapConcurrently<A, B>(f: (a: A) => Stream<B>, concurrency: number, s: Stream<A>): Stream<B>;
+export function mergeMapConcurrently<A, B>(f: (a: A) => Stream<B>): (concurrency: number, s: Stream<A>) => Stream<B>;
+export function mergeMapConcurrently<A, B>(f: (a: A) => Stream<B>, concurrency: number): (s: Stream<A>) => Stream<B>;
+export function mergeMapConcurrently<A, B>(f: (a: A) => Stream<B>): (concurrency: number) => (s: Stream<A>) => Stream<B>;
