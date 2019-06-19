d69d772f4df5f0b9fe5c970bdd01ce190dbb11d4
Brian Cavalier <brian@briancavalier.com>
2019-06-18 21:23:42 -0400

    Update many dev deps

--- examples/rollup.config.js
+++ examples/rollup.config.js
@@ -1,12 +1,18 @@
 import node from 'rollup-plugin-node-resolve'
 import flow from 'rollup-plugin-flow'
-import uglify from 'rollup-plugin-uglify'
+import { uglify } from 'rollup-plugin-uglify'
 import { minify } from 'uglify-es'
+import babel from 'rollup-plugin-babel'
 
 export default {
   plugins: [
     flow(),
     node(),
+    babel({
+      babelrc: false,
+      exclude: 'node_modules/**',
+      presets: [['env', { modules: false }]]
+    }),
     uglify({}, minify)
   ],
   output: {
