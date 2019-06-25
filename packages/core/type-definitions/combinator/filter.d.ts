0ac459750f95f1038219b948bacd6aee95804e61
Kirill Agalakov <agalakov@devexperts.com>
2019-06-25 16:03:04 +0300

    feat(core): support refinements in filter operator

--- packages/core/type-definitions/combinator/filter.d.ts
+++ packages/core/type-definitions/combinator/filter.d.ts
@@ -1,6 +1,8 @@
 import { Stream } from '@most/types';
 
+export function filter<A, B extends A>(p: (a: A) => a is B, s: Stream<A>): Stream<B>;
 export function filter<A>(p: (a: A) => boolean, s: Stream<A>): Stream<A>;
+export function filter<A, B extends A>(p: (a: A) => a is B): (s: Stream<A>) => Stream<B>;
 export function filter<A>(p: (a: A) => boolean): (s: Stream<A>) => Stream<A>;
 
 export function skipRepeats<A>(s: Stream<A>): Stream<A>;
